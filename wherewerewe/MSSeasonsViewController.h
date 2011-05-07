4412d4389d37e76af7dcf849221d7ff655f86713
Mark Scholtz <mark.scholtz@unboxedconsulting.com>
2011-05-07 08:34:36 +0200

    Add seasons and episodes view controllers

---
+++ wherewerewe/MSSeasonsViewController.h
@@ -0,0 +1,16 @@
+//
+//  MSSeasonsViewController.h
+//  wherewerewe
+//
+//  Created by Mark on 2011/05/07.
+//  Copyright 2011 Unboxed Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MSSeasonsViewController : UITableViewController {
+    
+}
+
+@end
